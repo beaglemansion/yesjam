263af203acf76b5ed590bfb95f9e9b8d3babdde9
beaglemansion <50973835+beaglemansion@users.noreply.github.com>
2021-10-17 15:24:47 +0900

    10월 2주차 2,3번째

---
+++ programmers/비밀지도.py
@@ -0,0 +1,27 @@
+# 비밀지도
+def get_row(n, arr):
+    new_arr = []
+    
+    for i in arr:
+        row = bin(i).split('0b')[1]
+        while len(row) < n:
+            row = '0' + row
+        new_arr.append(row)
+
+    return new_arr
+
+
+def solution(n, arr1, arr2):
+    answer = []
+    arr3 = get_row(n, arr1)
+    arr4 = get_row(n, arr2)
+
+    for i in range(n):
+        tmp = ''
+        for j in range(n): 
+            if int(arr3[i][j]) or int(arr4[i][j]):
+                tmp += '#'
+            else:
+                tmp += ' '
+        answer.append(tmp)
+    return answer
